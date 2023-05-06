8e655c42a3c512461b5e891a5b4dc12c4be209cf
Rafael da Silveira <rafael.dasilveira@auto1.com>
2023-05-06 18:42:09 +0200

    first commit

---
+++ app/src/main/java/com/github/xshifty/mapello/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.github.xshifty.mapello
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.util.Timer
+import java.util.TimerTask
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val mainAct = this
+
+        Timer().schedule(object : TimerTask() {
+            override fun run() {
+                startActivity(Intent(mainAct, LoginActivity::class.java))
+            }
+        }, 2000)
+    }
+}
